154107a664253b429625f452414b202e8586bf23
Harshul Rai <harshuljps@gmail.com>
2022-08-05 23:16:46 +0530

    started with backend

---
+++ routes/signup.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const controller = require('../controllers/signUpController');
+
+
+const router = express.Router();
+
+
+router.post('/signup', controller.signUp)
+
+module.exports = router
